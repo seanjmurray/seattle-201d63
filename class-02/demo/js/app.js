60783daac685b348a45ba3d123fd51ef4860e342
LENA EIVY <applena@gmail.com>
2020-04-28 12:09:24 -0700

    day 2 updated

---
+++ class-02/demo/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var watchedTigerKing = prompt('did you watch Tiger King?');
+
+// if they did watch Tiger King, I want to say 'Did Carol Baskin Kill her Husband?'
+// if they didn't watch Tiger King, I want to say 'Sorry'
+
+if(watchedTigerKing.toLowerCase() === 'yes'){
+  alert('Did Carol Baskin Kill her Husband?');
+} else if(watchedTigerKing.toLowerCase() === 'no'){
+  alert('Sorry');
+} else {
+  prompt('Please answer with a yes or a no');
+}
+
+// if(watchedTigerKing === 'yes'){
+//   // do something
+// }
+
+// if(watchedTigerKing === 'yes'){
+//   // do something
+// } else {
+//   // do something else
+// }
+
+// var likesCoffee = true;
+
+// if(likesCoffee){
+//   // do something
+// }
